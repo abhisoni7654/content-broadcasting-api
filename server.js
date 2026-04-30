68507777a75482ffe54d0cf861754932fc923aa2
Abhishek <b230961@skit.ac.in>
2026-04-30 18:02:46 +0530

    Serve API documentation PDF at root endpoint

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import dotenv from 'dotenv';
 import app from './src/app.js';
 import { connectDB } from './src/config/db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const startServer = async () => {
     await connectDB();
+
     app.get('/', (req, res) => {
-        res.send('Content broadcasting system');
+        const filePath = path.join(__dirname, 'API Docs.pdf');
+        res.setHeader('Content-Type', 'application/pdf');
+        res.sendFile(filePath);
     });
+
     app.listen(PORT, () => {
         console.log(`Server running on http://localhost:${PORT}`);
         console.log(`Health check: http://localhost:${PORT}/api/health`);
